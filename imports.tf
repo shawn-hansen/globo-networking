0e57e800c40a45013742148ee384a2aa4388907d
Hansen <shansen@ritchiebros.com>
2024-01-19 11:13:05 -0700

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+#### Imported on 1/18/24
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0f09615b56d9d3f23" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-04cbc416c3a96b314" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-05daee9f14b96fbd1" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-059f947a13f30bc7c" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-0300fa2a1e46bfd6e_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-0300fa2a1e46bfd6e" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-04cbc416c3a96b314/rtb-0300fa2a1e46bfd6e" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-05daee9f14b96fbd1/rtb-0300fa2a1e46bfd6e" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-0683081e2deb05831" #NoIngressSecurityGroup
+# }
